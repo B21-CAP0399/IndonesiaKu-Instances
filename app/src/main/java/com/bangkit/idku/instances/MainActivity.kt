bb714d8bc12d731897d7608df6361abee9e05f65
Leonards03 <edwin.leonards03@gmail.com>
2021-06-04 12:11:37 +0700

    initial init

---
+++ app/src/main/java/com/bangkit/idku/instances/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.bangkit.idku.instances
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dicoding.made.indonesiaku_instances.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
